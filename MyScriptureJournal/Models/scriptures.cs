6c43d5c3e7ff8a083a290fe57822065e30544bb3
Jersen Meim <meimjersen@gmail.com>
2020-10-28 14:04:31 +0800

    Validation  Added

--- MyScriptureJournal/Models/scriptures.cs
+++ MyScriptureJournal/Models/scriptures.cs
@@ -10,11 +10,19 @@ namespace MyScriptureJournal.Models
     {
         [Key]
         public int Scriptureid { get; set; }
+
+        [StringLength(30, MinimumLength = 3)]
         public string Book { get; set; }
+
+        [Range(1, 150)]
+        [StringLength(3, MinimumLength = 3)]
         [Display(Name = "Chapter/Section")]
         public int ChapterSection { get; set; }
         [Display(Name = "Verse/s")]
+        [Range(1, 150)]
         public int Verses { get; set; }
+
+        [StringLength(500, MinimumLength = 3)]
         public string Notes { get; set; }
         [Display(Name = "Date Created")]
         [DataType(DataType.Date)]
