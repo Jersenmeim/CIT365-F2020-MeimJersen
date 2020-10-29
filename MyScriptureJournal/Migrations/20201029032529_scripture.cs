89ea5f68b888f39f007d2eee376daa9b5ccb27c3
Jersen Meim <meimjersen@gmail.com>
2020-10-29 11:28:15 +0800

    add-migration

---
+++ MyScriptureJournal/Migrations/20201029032529_scripture.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyScriptureJournal.Migrations
+{
+    public partial class scripture : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "scriptures",
+                columns: table => new
+                {
+                    Scriptureid = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Book = table.Column<string>(maxLength: 30, nullable: true),
+                    ChapterSection = table.Column<int>(nullable: false),
+                    Verses = table.Column<int>(nullable: false),
+                    Notes = table.Column<string>(maxLength: 500, nullable: true),
+                    DateCreated = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_scriptures", x => x.Scriptureid);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "scriptures");
+        }
+    }
+}
