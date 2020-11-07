7c843e564f0d888c5dd2985c0ca2261546758b5f
Jersen Meim <meimjersen@gmail.com>
2020-11-07 21:00:58 +0800

    MVC Added (Week 9) | No styling Yet

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Meet the Mormons",
+                        ReleaseDate = DateTime.Parse("2014-10-10"),
+                        Genre = "Documentary",
+                        Rating = "PG",
+                        Price = 9.45M
+                        
+                    },
+
+                    new Movie
+                    {
+                        Title = "The R.M.",
+                        ReleaseDate = DateTime.Parse("2003-01-31"),
+                        Genre = "Comedy Religious",
+                        Rating = "G",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Other Side of Heaven",
+                        ReleaseDate = DateTime.Parse("2002-04-12"),
+                        Genre = "Drama Adventure",
+                        Rating = "PG",
+                        Price = 14.09M
+                        
+                        
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Saratov Approach",
+                        ReleaseDate = DateTime.Parse("2013-10-09"),
+                        Genre = "Drama Action",
+                        Rating = "PG-13",
+                        Price = 9.99M
+                        
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
