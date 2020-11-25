800d3b5ea27a2223bff1717d4bd86ac5944fc1d2
Jersen Meim <meimjersen@gmail.com>
2020-11-25 20:19:56 +0800

    BooksAPI Added

--- ContosoUniversity/Models/Person.cs
+++ ContosoUniversity/Models/Person.cs
@@ -9,12 +9,12 @@ namespace ContosoUniversity.Models
 
         [Required]
         [StringLength(50)]
-        [Display(Name = "Student Last Name")]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         [Required]
         [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         [Column("FirstName")]
-        [Display(Name = "Student First Name")]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
 
         [Display(Name = "Full Name")]
